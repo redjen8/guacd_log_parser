c4817e9037a1b584cda0a80b782209d930c36c5d
Seokwoo Jeong <seokwoo2525@gmail.com>
2021-08-23 14:13:15 +0900

    file flush and fsync to print sys buffer

--- log_parser.py
+++ log_parser.py
@@ -5,38 +5,20 @@ import select
 import time
 
 
-def classify_log(file_name):
-    directory = 'classified_logs'
-    # f = open(file_name, 'r')
-    # lines = f.readlines()
+def classify_log(file_name, dir_name):
+    directory = dir_name
     try:
         if not os.path.exists(directory):
             os.makedirs(directory)
     except OSError:
         print('Error : Creating Directory ' + directory)
 
-    # for line in lines:
-    #     pidLocationStart, pidLocationEnd = line.find('[')+1, line.find(']')
-    #     if pidLocationStart == -1 or pidLocationEnd == -1:
-    #         continue
-    #     pid = line[pidLocationStart:pidLocationEnd]
-    #     dateInfo = list(line[:6])
-    #     if dateInfo[4] == ' ':
-    #         dateInfo[4] = '0'
-    #     dateInfo[3] = '-'
-    #     dateInfo = "".join(dateInfo)
-    #     newFileName = directory + "/" + pid + "-" + dateInfo + ".log"
-    #     newFile = open(newFileName, 'a')
-    #     newFile.write(pid + ':')
-    #     newFile.write(line[pidLocationEnd + 2:] + '\n')
-    #     newFile.close()
-
     tf = subprocess.Popen(['tail', '-F', file_name],
                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     tp = select.poll()
     tp.register(tf.stdout)
     while True:
-        if tp.poll(1):
+        if tp.poll(0.001):
             line = tf.stdout.readline().decode()
             if not line:
                 break
@@ -54,8 +36,9 @@ def classify_log(file_name):
             newFile = open(newFileName, 'a')
             newFile.write(timeInfo + ' ' + pid + ':')
             newFile.write(line[pidLocationEnd + 2:] + '\n')
-            newFile.close()
-        time.sleep(0.001)
+            newFile.flush()
+            os.fsync(newFile)
+        time.sleep(0.1)
 
 
 def classify_log_test(istr):
@@ -67,13 +50,17 @@ def classify_log_test(istr):
 
 def main():
     file_name = sys.argv[1]
+    dir_name = sys.argv[2]
     if not file_name:
         print("please input guacd file name")
         sys.exit()
+    elif not dir_name:
+        print("please input directory name")
+        sys.exit()
     else:
-        classify_log(file_name)
+        classify_log(file_name, dir_name)
 
 
 if __name__ == "__main__":
-    # classify_log('guacd.log')
+    # classify_log('guacd.log', 'classified_log')
     main()
