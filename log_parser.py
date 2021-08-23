04d3486dbba5b2429b5e40d081b1b2b86a9d28b8
Seokwoo Jeong <seokwoo2525@gmail.com>
2021-08-23 11:36:05 +0900

    0.001 time scheduled tail guacd.log implemented

--- log_parser.py
+++ log_parser.py
@@ -1,27 +1,61 @@
 import sys
 import os
+import subprocess
+import select
+import time
 
 
 def classify_log(file_name):
-    f = open(file_name, 'r')
     directory = 'classified_logs'
-    lines = f.readlines()
+    # f = open(file_name, 'r')
+    # lines = f.readlines()
     try:
         if not os.path.exists(directory):
             os.makedirs(directory)
     except OSError:
         print('Error : Creating Directory ' + directory)
 
-    for line in lines:
-        pidLocationStart, pidLocationEnd = line.find('[')+1, line.find(']')
-        if pidLocationStart == -1 or pidLocationEnd == -1:
-            continue
-        pid = line[pidLocationStart:pidLocationEnd]
-        newFileName = directory + "/" + pid + ".log"
-        newFile = open(newFileName, 'a')
-        newFile.write(pid + ':')
-        newFile.write(line[pidLocationEnd + 2:] + '\n')
-        newFile.close()
+    # for line in lines:
+    #     pidLocationStart, pidLocationEnd = line.find('[')+1, line.find(']')
+    #     if pidLocationStart == -1 or pidLocationEnd == -1:
+    #         continue
+    #     pid = line[pidLocationStart:pidLocationEnd]
+    #     dateInfo = list(line[:6])
+    #     if dateInfo[4] == ' ':
+    #         dateInfo[4] = '0'
+    #     dateInfo[3] = '-'
+    #     dateInfo = "".join(dateInfo)
+    #     newFileName = directory + "/" + pid + "-" + dateInfo + ".log"
+    #     newFile = open(newFileName, 'a')
+    #     newFile.write(pid + ':')
+    #     newFile.write(line[pidLocationEnd + 2:] + '\n')
+    #     newFile.close()
+
+    tf = subprocess.Popen(['tail', '-F', file_name],
+                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    tp = select.poll()
+    tp.register(tf.stdout)
+    while True:
+        if tp.poll(1):
+            line = tf.stdout.readline().decode()
+            if not line:
+                break
+            pidLocationStart, pidLocationEnd = line.find('[')+1, line.find(']')
+            if pidLocationStart == -1 or pidLocationEnd == -1:
+                continue
+            pid = line[pidLocationStart:pidLocationEnd]
+            dateInfo = list(line[:6])
+            if dateInfo[4] == ' ':
+                dateInfo[4] = '0'
+            dateInfo[3] = '-'
+            dateInfo = "".join(dateInfo)
+            timeInfo = line[7:15]
+            newFileName = directory + "/" + pid + "-" + dateInfo + ".log"
+            newFile = open(newFileName, 'a')
+            newFile.write(timeInfo + ' ' + pid + ':')
+            newFile.write(line[pidLocationEnd + 2:] + '\n')
+            newFile.close()
+        time.sleep(0.001)
 
 
 def classify_log_test(istr):
@@ -41,4 +75,5 @@ def main():
 
 
 if __name__ == "__main__":
+    # classify_log('guacd.log')
     main()
